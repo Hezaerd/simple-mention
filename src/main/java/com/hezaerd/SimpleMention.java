0816396a708b6ffbb8057c9f00b78486cc8c58c9
Swann <swann.rouanet@gmail.com>
2025-07-22 17:07:26 +0200

    chore: create Fabric project

---
+++ src/main/java/com/hezaerd/SimpleMention.java
@@ -0,0 +1,10 @@
+package com.hezaerd;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SimpleMention implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/hezaerd/utils/ModLib.java
@@ -0,0 +1,11 @@
+package com.hezaerd.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModLib {
+    public static final String MOD_ID = "simple-mention";
+    public static final String MOD_NAME = "Simple Mention";
+    
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+}
